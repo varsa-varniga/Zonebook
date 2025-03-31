89894e9ec06082c8878119c454f35bdd3dc9a77e
sujithd24 <sujithd2424@gmail.com>
2025-03-31 14:57:40 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -11,21 +11,14 @@ db();
 app.use(express.json());  // Ensures server can read JSON requests
 app.use(cors());
 
-<<<<<<< HEAD
-=======
 // Routes
 const demoRoutes = require("./routes/demoRoutes");
 const updateRoutes = require("./routes/UpdateRoutes");
 app.use("/api/demo", demoRoutes);
 app.use("/api/update",updateRoutes);
 
->>>>>>> d4d848ddb1e2ccf2d55c1f9c179a6af4b580ca9b
 
 // Start Server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-<<<<<<< HEAD
-
-=======
->>>>>>> d4d848ddb1e2ccf2d55c1f9c179a6af4b580ca9b
